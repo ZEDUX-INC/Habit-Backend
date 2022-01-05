60b713817ba6304abb33672ce8a24373d6bc4c26
Joseph Adebowale <adebowalejojo2001@gmail.com>
2022-01-05 19:48:01 +0100

    commit

--- HabbitBackend/settings.py
+++ HabbitBackend/settings.py
@@ -14,14 +14,15 @@ import os
 import dj_database_url
 import environ
 
+# reading .env file
+environ.Env.read_env(".env")
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
 )
 
-# reading .env file
-environ.Env.read_env()
+env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
