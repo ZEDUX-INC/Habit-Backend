48f25c83bbeb2067bae3ba872745d39825962276
Joseph Adebowale <adebowalejojo2001@gmail.com>
2022-01-05 19:37:04 +0100

    commit

--- HabbitBackend/settings.py
+++ HabbitBackend/settings.py
@@ -36,7 +36,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://habbitapi.herokuapp.com/",
+    "127.0.0.1"
+]
 
 
 # Application definition
@@ -146,7 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'staticfiles')
 )
 
 #  Add configuration for static files storage using whitenoise
