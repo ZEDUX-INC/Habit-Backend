1bef7bf97147fc0c0bc54f1f5660a19d588286b0
Joseph Adebowale <adebowalejojo2001@gmail.com>
2022-03-08 17:48:45 +0100

    Implement follower user interaction

---
+++ account/api/v1/permissions.py
@@ -0,0 +1,11 @@
+from traceback import print_tb
+from rest_framework import permissions
+from account.models import CustomUser
+
+SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
+
+
+class IsUserOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(request.method in SAFE_METHODS) or bool(request.user == view.get_object())
--- tests/v1/account/test_serializers.py
+++ tests/v1/account/test_serializers.py
@@ -1,16 +1,20 @@
+import email
 import pytest
 
 from rest_framework.status import *
-from UserApp.models import CustomUser
-from tests.v1.userapp.test_models import UserFactory
+from account.models import CustomUser
+from tests.v1.account.test_models import UserFactory
 from tests.utils.TestCase import SerializerTestCase
 
-from APIV1.UserApp.serializers import (
+from account.api.v1.serializers import (
     RPEmailSerializer,
+    UserFollowerSerializer,
     UserSerializer,
     RPTokenSerializer,
     RPPasswordSerializer
 )
+
+from account.models import UserFollowing
 
 
 @pytest.mark.django_db
@@ -77,3 +81,54 @@ class TestRPPasswordSerializer(SerializerTestCase):
     def test_non_required_fields(self):
         self.check_non_required_fields(
             self.serializer, self.non_required_fields)
+
+
+@pytest.mark.django_db
+class TestUserFollowerSerializer(SerializerTestCase):
+    pytestmark = pytest.mark.django_db
+
+    def setUp(self) -> None:
+        self.user = UserFactory().create()
+        self.follower_user = UserFactory().create(email='user2@gmail.com')
+        self.serializer = UserFollowerSerializer
+
+        self.INVALID_DATA = [
+            {
+                'data': {'followed_user': self.follower_user.id},
+                'errors': {
+                    'followed_user': ['User is already being followed.']
+                },
+                'label': 'invalid follower'
+            },
+        ]
+
+        self.VALID_DATA = [
+            {'followed_user': self.follower_user.id},
+        ]
+        return super().setUp()
+
+    def test_valid_data(self) -> None:
+        self.check_valid_data(
+            self.serializer, entries=self.VALID_DATA, context={'user': self.user})
+
+    def test_invalid_data(self) -> None:
+        temp = UserFollowing.objects.create(
+            user=self.user, followed_user=self.follower_user)
+        self.check_invalid_data(
+            self.serializer, entries=self.INVALID_DATA, context={'user': self.user})
+        temp.delete()
+
+    def test_to_representation(self) -> None:
+        serializer = self.serializer(
+            data={'followed_user': self.follower_user.id}, context={'user': self.user})
+        serializer.is_valid()
+        follower_transaction = serializer.save()
+
+        data = {
+            'user': UserSerializer(instance=self.follower_user).data,
+            'following_since': follower_transaction.date_created,
+            'blocked': follower_transaction.blocked
+        }
+
+        self.assertDictEqual(data, serializer.data)
+        follower_transaction.delete()
