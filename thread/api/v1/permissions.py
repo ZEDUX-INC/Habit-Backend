bb448af52dd45c88af64def957bd534ed0e2dc1a
Joseph Adebowale <adebowalejojo2001@gmail.com>
2022-03-27 14:19:50 +0100

    Implement-Message

---
+++ thread/api/v1/permissions.py
@@ -0,0 +1,15 @@
+from typing import Any
+from rest_framework import permissions
+from rest_framework.request import Request
+from rest_framework.views import View
+
+
+SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
+
+
+class IsCreatorOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request: Request, view: View, obj: Any) -> bool:
+        if request.method in SAFE_METHODS:
+            return True
+        return (request.user == obj.created_by)
