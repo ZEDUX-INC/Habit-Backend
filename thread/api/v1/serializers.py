78c2a21997f3fb194a1935687d99494b8f779320
Joseph Adebowale <adebowalejojo2001@gmail.com>
2022-04-01 20:06:29 +0100

    Remove need to add created_by id to message and attachment serializers.

--- thread/api/v1/serializers.py
+++ thread/api/v1/serializers.py
@@ -1,13 +1,24 @@
+from typing import Dict, Any
 from rest_framework import serializers
+from django.db import DatabaseError
 from thread.models import Attachment, Message
 
 
 class AttachmentSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Attachment
         fields = '__all__'
-        read_only_fields = ['date_created']
+        read_only_fields = ['date_created', 'created_by']
+
+    def create(self, validated_data: Dict[str, Any]) -> Attachment:
+        user = self.context.get('request').user
+
+        obj = Attachment.objects.create(
+            **validated_data,
+            created_by=user
+        )
+
+        return obj
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -15,4 +26,14 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
-        read_only_fields = ['date_created', 'date_edited']
+        read_only_fields = ['date_created', 'date_edited', 'created_by']
+
+    def create(self, validated_data: Dict[str, Any]) -> Attachment:
+        user = self.context.get('request').user
+
+        obj = Message.objects.create(
+            **validated_data,
+            created_by=user
+        )
+
+        return obj
